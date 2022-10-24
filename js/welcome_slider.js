d7dc0ec216e1ef6f931338a706161d4e7b8b875d
Johan <grimmerw968@gmail.com>
2022-10-24 12:48:39 +0300

    refactor: spaces between code

--- js/welcome_slider.js
+++ js/welcome_slider.js
@@ -1,17 +1,17 @@
-const numberItem = document.querySelector(".num__item");
-const welcomeWrapper = document.querySelector(".welcome__img-wrapper");
+const numberItem = document.querySelector('.num__item');
+const welcomeWrapper = document.querySelector('.welcome__img-wrapper');
 //Текст контент работает
 
 let numberSlide = 1;
 
-const carouselItem = document.querySelectorAll(".carousel__item");
-const arrowsLeft = document.querySelector(".arrows__left");
-const arrowsRight = document.querySelector(".arrows__right");
+const carouselItem = document.querySelectorAll('.carousel__item');
+const arrowsLeft = document.querySelector('.arrows__left');
+const arrowsRight = document.querySelector('.arrows__right');
 
 //TODO: Slider
 
 const welcomeSlider = tns({
-  container: ".welcome__img-wrapper",
+  container: '.welcome__img-wrapper',
   items: 1,
   slideBy: 1,
   mouseDrag: true,
@@ -22,7 +22,7 @@ const welcomeSlider = tns({
 });
 
 const getNumberItem = () => {
-  return document.querySelector(".num__item");
+  return document.querySelector('.num__item');
 };
 
 const changeNumberItem = () => {
@@ -36,8 +36,8 @@ const prevSlide = () => {
   numberSlide = changeNumberItem();
 
   if (numberSlide === 1) {
-    numberItem.textContent = "05";
-    welcomeSlider.goTo("prev");
+    numberItem.textContent = '05';
+    welcomeSlider.goTo('prev');
     removeClassListActive();
     getSliderSpan((numberSlide = 5));
     return;
@@ -46,7 +46,7 @@ const prevSlide = () => {
     console.log(numberSlide);
     numberSlide -= 1;
     numberItem.textContent = `0${numberSlide}`;
-    welcomeSlider.goTo("prev");
+    welcomeSlider.goTo('prev');
     removeClassListActive();
     getSliderSpan(numberSlide);
     return;
@@ -57,8 +57,8 @@ const nextSlide = () => {
   numberSlide = changeNumberItem();
 
   if (numberSlide === 5) {
-    numberItem.textContent = "01";
-    welcomeSlider.goTo("next");
+    numberItem.textContent = '01';
+    welcomeSlider.goTo('next');
     removeClassListActive();
     getSliderSpan((numberSlide = 1));
     return;
@@ -66,47 +66,47 @@ const nextSlide = () => {
 
   removeClassListActive();
   getSliderSpan(numberSlide + 1);
-  welcomeSlider.goTo("next");
-  numberItem.textContent = "0" + `${numberSlide + 1}`;
+  welcomeSlider.goTo('next');
+  numberItem.textContent = '0' + `${numberSlide + 1}`;
 };
 
-arrowsLeft.addEventListener("click", prevSlide);
-arrowsRight.addEventListener("click", nextSlide);
+arrowsLeft.addEventListener('click', prevSlide);
+arrowsRight.addEventListener('click', nextSlide);
 
 const getSliderSpan = (getSlide) => {
-  carouselItem[getSlide - 1].classList.add("active");
+  carouselItem[getSlide - 1].classList.add('active');
 };
 //slider square
 carouselItem.forEach((x) =>
-  x.addEventListener("click", (event) => {
+  x.addEventListener('click', (event) => {
     removeClassListActive(x);
-    event.target.classList.add("active");
+    event.target.classList.add('active');
     slideNumb(event);
-    numberItem.textContent = "0" + (+event.target.id + 1);
+    numberItem.textContent = '0' + (+event.target.id + 1);
   })
 );
 
 const removeClassListActive = (element) => {
   carouselItem.forEach((x, index) => {
-    if (x.classList.contains("active")) x.classList.remove("active");
+    if (x.classList.contains('active')) x.classList.remove('active');
   });
 };
 
 const slideNumb = (event) => {
   const { target } = event;
-  if (target.id == "0") {
-    welcomeSlider.goTo("0");
+  if (target.id == '0') {
+    welcomeSlider.goTo('0');
   }
-  if (target.id == "1") {
-    welcomeSlider.goTo("1");
+  if (target.id == '1') {
+    welcomeSlider.goTo('1');
   }
-  if (target.id == "2") {
-    welcomeSlider.goTo("2");
+  if (target.id == '2') {
+    welcomeSlider.goTo('2');
   }
-  if (target.id == "3") {
-    welcomeSlider.goTo("3");
+  if (target.id == '3') {
+    welcomeSlider.goTo('3');
   }
-  if (target.id == "4") {
-    welcomeSlider.goTo("4");
+  if (target.id == '4') {
+    welcomeSlider.goTo('4');
   }
 };
